a0489a5508e4ef60b6dddb0339dd35140c7d844f
Cody Kremer <ckremer99@outlook.com>
2024-07-17 12:36:43 -0700

    displays pantry items

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 const User = require("../models/user.js");
 
-router.get("/", (req, res) => {
-  res.render("foods/index.ejs", {user: req.session.user});
+router.get("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id)
+    const pantryItems = currentUser.pantry
+    res.render("foods/index.ejs", {
+      user: req.session.user,
+      pantry: pantryItems});
+
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 });
 
 router.get("/new", (req, res) => {
@@ -17,7 +27,7 @@ router.post("/", async (req, res) => {
     const currentUser = await User.findById(req.session.user._id)
     currentUser.pantry.push(req.body)
     await currentUser.save()
-    console.log(currentUser)
+    // console.log(currentUser)
     res.redirect(`/users/${currentUser._id}/foods`)
 
   } catch (error) {
